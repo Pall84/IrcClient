ab27a33fb79332a1e952dd643d3a8ceffd77b11f
Palli <palle10@ru.is>
2012-11-17 18:15:01 +0000

    einfaldið process_long_server_command og sparaði nokkuð hundruð línur af kóða. bætti við ison.

--- IrcClient.py
+++ IrcClient.py
@@ -133,7 +133,7 @@ class IrcClient(object):
         commands_without_trailer = 'NICK', 'JOIN', 'PART', 'NAMES', 'TRACE',\
                                    'MODE', 'LIST', 'INVITE', 'KICK',\
                                    'VERSION', 'STATS', 'LINKS', 'TIME', 'ADMIN',\
-                                   'INFO', 'WHO', 'WHOIS', 'WHOWAS'
+                                   'INFO', 'WHO', 'WHOIS', 'WHOWAS', 'ISON'
 
         commands_with_trailer = 'PRIVMSG', 'NOTICE', 'TOPIC'
 
@@ -203,212 +203,73 @@ class IrcClient(object):
         else:
             print message
     def __process_irc_long_server_command(self, message):
+
+        irc_command = ['001', 'RPL_WELCOME'], ['002', 'RPL_YOURHOST'], ['003', 'RPL_CREATED'],\
+                      ['004', 'RPL_MYINFO'], ['005', 'RPL_ISUPPORT'], ['042', 'RPL_YOURID'],\
+                      ['200', 'RPL_TRACELINK'], ['201', 'RPL_TRACECONNECTING'], ['202', 'RPL_TRACEHANDSHAKE'],\
+                      ['203', 'RPL_TRACEUNKNOWN'], ['204', 'RPL_TRACEOPERATOR'], ['205', 'RPL_TRACEUSER'],\
+                      ['206', 'RPL_TRACESERVER'], ['208', 'RPL_TRACENEWTYPE'], ['211', 'RPL_STATSLINKINFO'],\
+                      ['212', 'RPL_STATSCOMMANDS'], ['213', 'RPL_STATSCLINE'], ['214', 'RPL_STATSNLINE'],\
+                      ['215', 'RPL_STATSILINE'], ['216', 'RPL_STATSKLINE'], ['217', 'RPL_STATSQLINE'],\
+                      ['218', 'RPL_STATSYLINE'], ['219', 'RPL_ENDOFSTATS'], ['221', 'RPL_UMODEIS'],\
+                      ['231', 'RPL_SERVICEINFO'], ['232', 'RPL_ENDOFSERVICES'], ['233', 'RPL_SERVICE'],\
+                      ['241', 'RPL_STATSLLINE'], ['242', 'RPL_STATSUPTIME'], ['243', 'RPL_STATSOLINE'],\
+                      ['244', 'RPL_STATSHLINE'], ['244', 'RPL_STATSHLINE'], ['251', 'RPL_LUSERCLIENT'],\
+                      ['252', 'RPL_LUSEROP'], ['253', 'RPL_LUSERUNKNOWN'], ['254', 'RPL_LUSERCHANNELS'],\
+                      ['255', 'RPL_LUSERME'], ['256', 'RPL_ADMINME'], ['257', 'RPL_ADMINLOC1'],\
+                      ['258', 'RPL_ADMINLOC2'], ['259', 'RPL_ADMINEMAIL'], ['261', 'RPL_TRACELOG'],\
+                      ['262', 'RPL_TRACEEND'], ['265', 'RPL_LOCALUSERS'], ['266', 'RPL_GLOBALUSERS'],\
+                      ['300', 'RPL_NONE'], ['301', 'RPL_AWAY'], ['302', 'RPL_USERHOST'], ['303', 'RPL_ISON'],\
+                      ['305', 'RPL_UNAWAY'], ['306', 'RPL_NOWAWAY'], ['311', 'RPL_WHOISUSER'],\
+                      ['312', 'RPL_WHOISSERVER'], ['313', 'RPL_WHOISOPERATOR'], ['314', 'RPL_WHOWASUSER'],\
+                      ['315', 'RPL_ENDOFWHO'], ['316', 'RPL_WHOISCHANOP'], ['317', 'RPL_WHOISIDLE'],\
+                      ['318', 'RPL_ENDOFWHOIS'], ['319', 'RPL_WHOISCHANNELS'], ['321', 'RPL_LISTSTART'],\
+                      ['322', 'RPL_LIST'], ['323', 'RPL_LISTEND'], ['324', 'RPL_CHANNELMODEIS'],\
+                      ['331', 'RPL_NOTOPIC'], ['332', 'RPL_TOPIC'], ['341', 'RPL_INVITING'], \
+                      ['342', 'RPL_SUMMONING'], ['351', 'RPL_VERSION'], ['352', 'RPL_WHOREPLY'],\
+                      ['353', 'RPL_NAMREPLY'], ['361', 'RPL_KILLDONE'], ['362', 'RPL_CLOSING'],\
+                      ['363', 'RPL_CLOSEEND'], ['364', 'RPL_LINKS'], ['365', 'RPL_ENDOFLINKS'],\
+                      ['366', 'RPL_ENDOFNAMES'], ['367', 'RPL_BANLIST'], ['368', 'RPL_ENDOFBANLIST'],\
+                      ['369', 'RPL_ENDOFWHOWAS'], ['371', 'RPL_INFO'], ['372', 'RPL_MOTD'],\
+                      ['373', 'RPL_INFOSTART'], ['374', 'RPL_ENDOFINFO'], ['375', 'RPL_MOTDSTART'],\
+                      ['376', 'RPL_ENDOFMOTD'], ['381', 'RPL_YOUREOPER'], ['382', 'RPL_REHASHING'],\
+                      ['384', 'RPL_MYPORTIS'], ['391', 'RPL_TIME'], ['392', 'RPL_USERSSTART'],\
+                      ['393', 'RPL_USERS'], ['394', 'RPL_ENDOFUSERS'], ['395', 'RPL_NOUSERS'],\
+                      ['401', 'ERR_NOSUCHNICK'], ['402', 'ERR_NOSUCHSERVER'], ['403', 'ERR_NOSUCHCHANNEL'],\
+                      ['404', 'ERR_CANNOTSENDTOCHAN'], ['405', 'ERR_TOOMANYCHANNELS'], ['406', 'ERR_WASNOSUCHNICK'],\
+                      ['407', 'ERR_TOOMANYTARGETS'], ['409', 'ERR_NOORIGIN'], ['411', 'ERR_NORECIPIENT'],\
+                      ['412', 'ERR_NOTEXTTOSEND'], ['413', 'ERR_NOTOPLEVEL'], ['414', 'ERR_WILDTOPLEVEL'],\
+                      ['421', 'ERR_UNKNOWNCOMMAND'], ['422', 'ERR_NOMOTD'], ['423', 'ERR_NOADMININFO'],\
+                      ['424', 'ERR_FILEERROR'], ['431', 'ERR_NONICKNAMEGIVEN'], ['432', 'ERR_ERRONEUSNICKNAME'],\
+                      ['433', 'ERR_NICKNAMEINUSE'], ['436', 'ERR_NICKCOLLISION'], ['441', 'ERR_USERNOTINCHANNEL'],\
+                      ['442', 'ERR_NOTONCHANNEL'], ['443', 'ERR_USERONCHANNEL'], ['444', 'ERR_NOLOGIN'],\
+                      ['445', 'ERR_SUMMONDISABLED'], ['446', 'ERR_USERSDISABLED'], ['461', 'ERR_NEEDMOREPARAMS'],\
+                      ['462', 'ERR_ALREADYREGISTERED'], ['463', 'ERR_NOPERMFORHOST'], ['464', 'ERR_PASSWDMISMATCH'],\
+                      ['465', 'ERR_YOUREBANNEDCREEP'], ['466', 'ERR_YOUWILLBEBANNED'], ['466', 'ERR_KEYSET'],\
+                      ['471', 'ERR_CHANNELISFULL'], ['472', 'ERR_UNKNOWNMODE'], ['473', 'ERR_INVITEONLYCHAN'],\
+                      ['474', 'ERR_BANNEDFROMCHAN'], ['475', 'ERR_BADCHANNELKEY'], ['481', 'ERR_NOPRIVILEGES'],\
+                      ['482', 'ERR_CHANOPRIVSNEEDED'], ['483', 'ERR_CANTKILLSERVER'], ['491', 'ERR_NOOPERHOST'],\
+                      ['492', 'ERR_NOSERVICEHOST'], ['502', 'ERR_USERSDONTMATCH'], ['502', 'ERR_USERSDONTMATCH'],\
+                      ['MODE', 'MODE'], ['TOPIC', 'TOPIC'],
+
+
         # parse message into command and parameter list.
         message = self.__parse_message(message)
 
-        # retrieve command from message.
+       # retrieve command from message.
         command = message[1][0]
 
         # check what kind of command and respond.
-        if command == '001' or command == 'RPL_WELCOME':
-            # retrieve message, print and log it.
-            message = ' '.join(message[1][2:])
-            print message
-            self.__log_message('server', 'RPL_WELCOME '+message)
+        for c in irc_command:
+            if command == c[0] or command == c[1]:
+                # retrieve message, print and log it.
+                message = ' '.join(message[1][2:])
+                print message
+                self.__log_message('server', c[1]+' '+message)
+                return
 
-        elif command == '002' or command == 'RPL_YOURHOST':
-            # retrieve message, print and log it.
-            message = ' '.join(message[1][2:])
-            print message
-            self.__log_message('server', 'RPL_YOURHOST '+message)
-
-        elif command == '003' or command == 'RPL_CREATED':
-            # retrieve message, print and log it.
-            message = ' '.join(message[1][2:])
-            print message
-            self.__log_message('server', 'RPL_CREATED '+message)
-
-        elif command == '004' or command == 'RPL_MYINFO':
-            # retrieve message, print and log it.
-            message = ' '.join(message[1][2:])
-            print message
-            self.__log_message('server', 'RPL_MYINFO '+message)
-
-        elif command == '005' or command == 'RPL_ISUPPORT ':
-            # retrieve message, print and log it.
-            message = ' '.join(message[1][2:])
-            print message
-            self.__log_message('server', 'RPL_ISUPPORT  '+message)
-
-        elif command == '042' or command == 'RPL_YOURID ':
-            # retrieve message, print and log it.
-            message = ' '.join(message[1][2:])
-            print message
-            self.__log_message('server', 'RPL_YOURID  '+message)
-
-        elif command == '200' or command == 'RPL_TRACELINK ':
-            # retrieve message, print and log it.
-            message = ' '.join(message[1][2:])
-            print message
-            self.__log_message('server', 'RPL_TRACELINK  '+message)
-
-        elif command == '201' or command == 'RPL_TRACECONNECTING ':
-            # retrieve message, print and log it.
-            message = ' '.join(message[1][2:])
-            print message
-            self.__log_message('server', 'RPL_TRACECONNECTING  '+message)
-
-        elif command == '202' or command == 'RPL_TRACEHANDSHAKE ':
-            # retrieve message, print and log it.
-            message = ' '.join(message[1][2:])
-            print message
-            self.__log_message('server', 'RPL_TRACEHANDSHAKE  '+message)
-
-        elif command == '203' or command == 'RPL_TRACEUNKNOWN ':
-            # retrieve message, print and log it.
-            message = ' '.join(message[1][2:])
-            print message
-            self.__log_message('server', 'RPL_TRACEUNKNOWN  '+message)
-
-        elif command == '204' or command == 'RPL_TRACEOPERATOR ':
-            # retrieve message, print and log it.
-            message = ' '.join(message[1][2:])
-            print message
-            self.__log_message('server', 'RPL_TRACEOPERATOR  '+message)
-
-        elif command == '205' or command == 'RPL_TRACEUSER ':
-            # retrieve message, print and log it.
-            message = ' '.join(message[1][2:])
-            print message
-            self.__log_message('server', 'RPL_TRACEUSER  '+message)
-
-        elif command == '206' or command == 'RPL_TRACESERVER ':
-            # retrieve message, print and log it.
-            message = ' '.join(message[1][2:])
-            print message
-            self.__log_message('server', 'RPL_TRACESERVER  '+message)
-
-        elif command == '251' or command == 'RPL_LUSERCLIENT ':
-            # retrieve message, print and log it.
-            message = ' '.join(message[1][2:])
-            print message
-            self.__log_message('server', 'RPL_LUSERCLIENT  '+message)
-
-        elif command == '252' or command == 'RPL_LUSEROP ':
-            # retrieve message, print and log it.
-            message = ' '.join(message[1][2:])
-            print message
-            self.__log_message('server', 'RPL_LUSEROP  '+message)
-
-        elif command == '254' or command == 'RPL_LUSERCHANNELS ':
-            # retrieve message, print and log it.
-            message = ' '.join(message[1][2:])
-            print message
-            self.__log_message('server', 'RPL_LUSERCHANNELS  '+message)
-
-        elif command == '255' or command == 'RPL_LUSERME ':
-            # retrieve message, print and log it.
-            message = ' '.join(message[1][2:])
-            print message
-            self.__log_message('server', 'RPL_LUSERME  '+message)
-
-        elif command == '262' or command == 'RPL_TRACEEND ':
-            # retrieve message, print and log it.
-            message = ' '.join(message[1][2:])
-            print message
-            self.__log_message('server', 'RPL_TRACEEND  '+message)
-
-        elif command == '265' or command == 'RPL_LOCALUSERS ':
-            # retrieve message, print and log it.
-            message = ' '.join(message[1][2:])
-            print message
-            self.__log_message('server', 'RPL_LOCALUSERS  '+message)
-
-        elif command == '266' or command == 'RPL_GLOBALUSERS ':
-            # retrieve message, print and log it.
-            message = ' '.join(message[1][2:])
-            print message
-            self.__log_message('server', 'RPL_GLOBALUSERS  '+message)
-
-        elif command == '353' or command == 'RPL_NAMREPLY ':
-            # retrieve message, print and log it.
-            message = ' '.join(message[1][3:])
-            print message
-            self.__log_message('server', 'RPL_NAMREPLY  '+message)
-
-        elif command == '366' or command == 'RPL_ENDOFNAMES ':
-            # retrieve message, print and log it.
-            message = ' '.join(message[1][2:])
-            print message
-            self.__log_message('server', 'RPL_ENDOFNAMES  '+message)
-
-        elif command == '375' or command == 'RPL_MOTDSTART ':
-            # retrieve MOTD message, print and log it.
-            message = ' '.join(message[1][2:])
-            print message
-            self.__log_message('server', 'RPL_MOTDSTART  '+message)
-
-            # add start of MOTD to clients motd.
-            self.motd = message+'\n'
-
-        elif command == '372' or command == 'RPL_MOTD ':
-            # retrieve MOTD message, print and log it.
-            message = ' '.join(message[1][2:])
-            print message
-            self.__log_message('server', 'RPL_MOTD  '+message)
-
-            # add MOTD to clients motd.
-            self.motd += message+'\n'
-
-        elif command == '376' or command == 'RPL_ENDOFMOTD ':
-            # retrieve MOTD message, print and log it.
-            message = ' '.join(message[1][2:])
-            print message
-            self.__log_message('server', 'RPL_ENDOFMOTD  '+message)
-
-        elif command == '401' or command == 'ERR_NOSUCHNICK ':
-            # retrieve error message, print and log it.
-            message = ' '.join(message[1][2:])
-            print message
-            self.__log_message('server', 'ERR_NOSUCHNICK  '+message)
-
-        elif command == '402' or command == 'ERR_NOSUCHSERVER ':
-            # retrieve error message, print and log it.
-            message = ' '.join(message[1][2:])
-            print message
-            self.__log_message('server', 'ERR_NOSUCHSERVER  '+message)
-
-        elif command == '403' or command == 'ERR_NOSUCHCHANNEL ':
-            # retrieve error message, print and log it.
-            message = ' '.join(message[1][2:])
-            print message
-            self.__log_message('server', 'ERR_NOSUCHCHANNEL  '+message)
-
-        elif command == '461' or command == 'ERR_NEEDMOREPARAMS ':
-            # retrieve error message, print and log it.
-            message = ' '.join(message[1][2:])
-            print message
-            self.__log_message('server', 'ERR_NEEDMOREPARAMS  '+message)
-
-            # add end of MOTD to clients motd.
-            self.motd += message
-
-        elif command == 'MODE':
-            # retrieve mode message, print and log it.
-            message = ' '.join(message[1][2:])
-            print message
-            self.__log_message('server', 'MODE  '+message)
-
-            # set clients mode to message.
-            self.mode = message
-
-        elif command == 'NOTICE':
-            # retrieve notice message, print and log it.
-            message = ' '.join(message[1][2:])
-            print message
-            self.__log_message('server', 'NOTICE  '+message)
-
-        elif command == 'NICK':
+        # check what kind of command and respond.
+        if command == 'NICK':
             self.nickname = message[1][1]
 
             # retrieve nick name, print and log it.
@@ -427,6 +288,12 @@ class IrcClient(object):
             message = '%s just left %s' %(message[0], message[1][1])
             print message
             self.__log_message('server','PART '+message)
+
+        elif command == 'NOTICE':
+            # retrieve nick name, print and log it.
+            message = '%s : %s' %(message[1][1], message[1][2])
+            print message
+            self.__log_message('server','NOTICE '+message)
 
         elif command == 'PRIVMSG':
             #check if this is ctcp message
